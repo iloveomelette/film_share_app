1a8f30725b1c04220026fd6eb87859b5e7ffd721
iloveomelette <mizuki1998hosei@gmail.com>
2022-03-10 13:56:29 +0900

    詳細表示機能の実装とスタイリング:ver0

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,9 @@ class PostsController < ApplicationController
     @posts = Post.order(created_at: :desc)
   end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+  end
 
   def new
     @post = Post.new
